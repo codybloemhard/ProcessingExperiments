863c39ac9842142e1e08df9804d09d663806406c
Cody Bloemhard <cblmhrd@live.nl>
2018-08-10 00:37:48 +0200

    predator cellular automata started

--- ParticleSim/ParticleSim/ParticleSim.js
+++ ParticleSim/ParticleSim/ParticleSim.js
@@ -5,37 +5,55 @@ var energyColourStrength = 0.01;
 
 var colours = [];
 
-var field;
-var realtime = true;
+var field, nfield;
+var realtime = false;
 
 function setup() {
   randomSeed(666);
-  frameRate(10);
+  frameRate(60);
   createCanvas(ww, wh);
   field = CreateField(gw, gh);
+  nfield = CreateField(gw, gh);
   colours['Z'] = [255, 255, 255];
   colours['A'] = [255, 0, 0];
+  colours['B'] = [0, 255, 0];
+  colours['C'] = [0, 0, 255];
   
-  field[gw/2][gh/2] = new Cell('Z', 1000);
+  field[20][20] = new Cell('A', 0);
+  field[gw - 20][20] = new Cell('B', 0);
+  field[gw/2][gh-20] = new Cell('C', 0);
 }
 
 function draw() {
+  //make walls
+  if(mouseIsPressed){
+    var xx = Math.floor(mouseX / gs);
+    var yy = Math.floor(mouseY / gs);
+    field[xx][yy] = new Cell('Z', 0);
+    if(!realtime) Render();
+  }
   if(!realtime) return;
   //Updating
   for(var x = 0; x < gw; x++)
-  for(var y = 0; y < gh; y++){
+  for(var y = 0; y < gh; y++)
     field[x][y].Update(x, y);
-  }
+  var temp = field;
+  field = nfield;
+  nfield = temp;
+  ZeroField(nfield);
   //Drawing
+  Render();
+}
+
+function Render(){
   background(0);
   noStroke();
   var m = '0';
   var str = 0;
   var col = [0,0,0]
   for(var x = 0; x < gw; x++)
-  for(var y = 0; y < gh; y++){
+  for(var y = 0; y < gh; y++)
     field[x][y].Draw(x, y);
-  }
 }
 
 function keyPressed() {
@@ -48,30 +66,39 @@ function Cell(m, e){
   this.energy = e;
   
   this.Update = function(x, y){
-     if(this.mode === '0') return;
-     if(this.energy <= 3){
-       this.mode = '0';
+     if(this.mode === 'Z'){
+       nfield[x][y] = new Cell('Z', 0);
        return;
      }
-     var enA = this.energy / 4;
-     var enB = this.energy - (3*enA);
-     
-     var neigh = new Array(4);
-     neigh[0] = field[edgeX(x - 1)][y];
-     neigh[1] = field[edgeX(x + 1)][y];
-     neigh[2] = field[x][edgeY(y - 1)];
-     neigh[3] = field[x][edgeY(y + 1)];
-     
-     neigh[0].mode = this.mode;
-     neigh[0].energy += enB;
-     for(var i = 1; i < 4; i++){
-       neigh[i].mode = this.mode;
-       neigh[i].energy += enA;
+     var surroundings = new Array(4);
+     surroundings[0] = field[edgeX(x - 1)][y];
+     surroundings[1] = field[edgeX(x + 1)][y];
+     surroundings[2] = field[x][edgeY(y - 1)];
+     surroundings[3] = field[x][edgeY(y + 1)];
+     if(this.mode === '0'){
+       for(var i = 0; i < 4; i++){
+         if(surroundings[i].mode != '0' && surroundings[i].mode != 'Z')
+           nfield[x][y] = new Cell(surroundings[i].mode, 0);
+       }
+       return; 
      }
-     
-     this.energy = 0;
+     var eaten = false;
+     var em = this.mode;
+     for(var i = 0; i < 4; i++){
+        var sm = surroundings[i].mode;
+        var diff = sm - this.mode
+        if((em == 'A' && sm == 'B') || (em == 'B' && sm == 'C') || (em == 'C' && sm == 'A')){//diff == 1 || diff == (3 - 1)
+          eaten = true;
+          em = sm;
+          break;
+        }
+     }
+     if(eaten)
+       nfield[x][y] = new Cell(em, 0);
+     else
+       nfield[x][y] = new Cell(this.mode, 0);
   }
-  
+   
   this.Draw = function(x, y){
     if(this.mode === '0') return;
     var col = colours[this.mode]
@@ -83,6 +110,14 @@ function Cell(m, e){
   }
 }
 
+function ZeroField(f){
+  for(var i = 0; i < f.length; i++)
+  for(var j = 0; j < f[i].length; j++){
+      f[i][j][0] = '0';
+      f[i][j][0] = 0;
+  }
+}
+
 function CreateField(w, h){
   w = Math.floor(w);
   h = Math.floor(h);
@@ -91,8 +126,7 @@ function CreateField(w, h){
     x[k] = new Array(h);
   for(var i = 0; i < w; i++)
   for(var j = 0; j < h; j++)
-   x[i][j] = new Cell('0', 0);
-  
+    x[i][j] = new Cell('0', 0);
   return x;
 }
 
